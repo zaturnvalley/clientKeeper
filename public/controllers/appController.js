2ade8c6f8e25987ade66dd76aa63053a4171f28e
zaturnvalley <zacharycmason@gmail.com>
2016-11-02 18:13:00 -0700

    pulled in clients via mongo through angular module

--- public/controllers/appController.js
+++ public/controllers/appController.js
@@ -1,5 +1,8 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
-
+  $http.get('/clients').success(function(res){
+    console.log('Data Received From Server');
+    $scope.clients = res;
+  });
 }]);
